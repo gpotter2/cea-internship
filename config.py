78dc269c51fc0a761ac7d4b07b60a16e1834df73
gpotter2 <gabriel@potter.fr>
2021-06-16 18:39:05 +0200

    Now expand !

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Config
 zlength = -1  # Crop the current image to zlength in z. Negative for no cropping (big size)
-MAX_TIME = 1  # How many instants we propagate
-dz = -5  # How much we move on each instant
+MAX_TIME = 10  # How many instants we propagate
+dz = -10  # How much we move on each instant
 
 time_drop = 4  # How much we drop of time precision for the visualisation
 
