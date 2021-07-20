19479eb0247d9785a6fdafda75ec44b3b669862c
gpotter2 <gabriel@potter.fr>
2021-07-20 16:43:59 +0200

    Improve stacking

--- paraview_source/main.py
+++ paraview_source/main.py
@@ -14,11 +14,11 @@ from source import getSource
 
 getSource(SUFFIX="low",
           LOG_SCALE=True, LOG_THRESHOLD=1e-04,
-          THRESHOLD=4.2,
-          CLIM=6,
+          THRESHOLD=3.8,
+          CLIM=5.7,
           OPACITY=0.01, COLOR="GYPi")
 getSource(SUFFIX="high",
           LOG_SCALE=True, LOG_THRESHOLD=4e-04,
-          THRESHOLD=4.2,
+          THRESHOLD=4.5,
           CLIM=6,
-          OPACITY=0.2)
+          OPACITY=0.3)
