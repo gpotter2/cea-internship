107bd2c9376f70c7cd754633f10321fc7ea3d55a
gpotter2 <gabriel@potter.fr>
2021-07-20 14:52:35 +0200

    filer > 4.5

--- process_data/high_and_low.sh
+++ process_data/high_and_low.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PROPAGATE_PROFILE='high' python propagate.py --filter-highpass 2.5 --suffix high
+PROPAGATE_PROFILE='high' python propagate.py --filter-highpass 4.5 --suffix high
 python propagate.py --filter-lowpass 1.5 --suffix low
